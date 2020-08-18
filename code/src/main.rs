2a17f33fb8f56f2b7f3706d52b865201b79755f8
jongwhan lee <jonghwan@crypto.com>
2020-08-18 10:37:52 +0800

    add bind

--- code/src/main.rs
+++ code/src/main.rs
@@ -1,30 +1,6 @@
-type UserFunc = Box<dyn Fn(u64, String)->bool>;
-
-fn check(myfunc: UserFunc)
+fn main()
 {
-  
-    for i in 0..3 {
-        println!("=======  {}", i);
-        let r= myfunc(i,format!("my number {}",i));
-        println!("result {}", r);
-    }
-
-}
-
-fn main() {
-    let user_func = | number:u64, info:String| -> bool {
-        let a= quest::ask(format!("{}  {}   proceed=", number, info).as_str());
-        let b= quest::yesno(true).unwrap().unwrap();
-        b
-    };
-
-    check(Box::new(user_func));
- 
-   /* check(
-
-    );
-    let a= quest::ask("proceed=");
-    let b= quest::yesno(true);
-    println!("{:?}", b);*/
-
+    let a= std::time::Duration::from_secs(1000);
+    let b= a/3;
+    println!("{:?} {:?}", a,b);
 }
--- test_unixsocket/src/main.rs
+++ test_unixsocket/src/main.rs
@@ -1,37 +1,7 @@
-use std::io::{BufRead, BufReader};
-use std::os::unix::net::{UnixStream,UnixListener};
-use std::thread;
+mod bind;
 
-fn handle_client(stream: UnixStream) {
-    let stream = BufReader::new(stream);
-    for line in stream.lines() {
-        println!("{}", line.unwrap());
-    }
-}
-
-fn test_connect()
+fn main()
 {
-    loop {
-        std::thread::sleep(std::time::Duration::from_secs(1));
-       // println!("client..");
-    }
-}
-fn main() {
-    let listener = UnixListener::bind("/tmp/my_uds.sock").unwrap();
+    bind::main();
 
-    thread::spawn(|| {
-        test_connect();
-
-    });
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                thread::spawn(|| handle_client(stream));
-            }
-            Err(err) => {
-                println!("Error: {}", err);
-                break;
-            }
-        }
-    }
 }
